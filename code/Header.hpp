7253ee8da1ab4b146f36df51b1696a093bb15bd3
starHo4 <hi6yukiho4@gmail.com>
2022-05-13 08:46:42 +0900

    Create Header.hpp

---
+++ code/Header.hpp
@@ -0,0 +1,28 @@
+#ifndef __Header__
+#define __Header__
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <filesystem>
+#include <fstream>
+#include <memory>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+// Define PI
+#define PI M_PI
+
+// Global Consts for Reports
+#define FILEPATH "/data/"
+#define FILEPATH_GENOME "/genome/"
+#define SAVEMOVIE true
+#define SAVEMOVIE_INTERVAL 100
+#define SAVEPOPUL_INTERVAL 100
+#define SAVECHROM_INTERVAL 1000
+
+// Include Parameters
+
+#endif
